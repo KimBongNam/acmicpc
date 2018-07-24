b025db3afccc1fa570e0d4f187acb83c29e5dace
DESKTOP-8TI980E\vbnmz <dragonight1@naver.com>
2018-07-24 23:01:03 +0900

    no message

---
+++ 10828_stack.py
@@ -0,0 +1,33 @@
+stack = []
+num = int(input())
+
+for i in range(num):
+   order =  input()
+   order_l = order.split(" ")
+   if len(order_l) == 2:
+      if order_l[0] == 'push':
+         stack.append(order_l[1])
+
+   else:
+      if order_l[0] == 'pop':
+         if len(stack)==0:
+            print('-1')
+         else:
+            print(stack[len(stack)-1])
+            del stack[len(stack)-1]
+      elif order_l[0] == 'size':
+         print(len(stack))
+
+      elif order_l[0] == 'empty':
+         if len(stack) == 0:
+            print('1')
+         else:
+            print('0')
+
+      elif order_l[0] == 'top':
+         if len(stack) == 0:
+            print('-1')
+         else:
+            print(stack[len(stack)-1])
+            
+            
---
+++ 10845_queue = [].py
@@ -0,0 +1,38 @@
+queue = []
+num = int(input())
+
+for i in range(num):
+   order =  input()
+   order_l = order.split(" ")
+   if len(order_l) == 2:
+      if order_l[0] == 'push':
+         queue.append(order_l[1])
+
+   else:
+      if order_l[0] == 'pop':
+         if len(queue)==0:
+            print('-1')
+         else:
+            print(queue[0])
+            del queue[0]
+      elif order_l[0] == 'size':
+         print(len(queue))
+
+      elif order_l[0] == 'empty':
+         if len(queue) == 0:
+            print('1')
+         else:
+            print('0')
+
+      elif order_l[0] == 'front':
+         if len(queue) == 0:
+            print('-1')
+         else:
+            print(queue[0])
+      elif order_l[0] == 'back':
+         if len(queue) == 0:
+            print('-1')
+         else:
+            print(queue[len(queue)-1])
+            
+            
