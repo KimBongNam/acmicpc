4388a1f01cdf9c366b970ca61973aa7ce68b3e51
Kimbongnam <dragonight1@naver.com>
2019-10-01 13:52:50 +0900

    no message

---
+++ 2156..py
@@ -0,0 +1,16 @@
+import sys
+
+inp = list(map(int,sys.stdin.readlines()))
+num = inp[0]
+wine = inp[1:]
+dp = [0]
+for i in range(num):
+    if i < 2:
+        dp.append(dp[i]+wine[i])
+    elif i == 2:
+        dp.append(max(wine[i-2]+wine[i], wine[i-1]+wine[i],dp[i]))
+    else:
+        dp.append(max(dp[i],dp[i-1] + wine[i],dp[i-2]+wine[i-1]+wine[i]))
+    
+print(dp[num])
+print(dp)
