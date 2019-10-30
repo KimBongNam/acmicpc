1c71493b9eac28efec01e98e08af94b569a958de
BongnamKim <vbnmzx1@naver.com>
2019-10-30 17:58:18 +0900

    no message

--- 1260.py
+++ 1260.py
@@ -1,29 +1,23 @@
 import sys
 
 
-def dfs(admat, start, node):
-    lst = []
+def dfs(admat, start, lst):
     lst.append(start)
-    or_start=start
+    for j in range(1,node+1):
+        if admat[start][j] == 1 and j not in lst:
+            lst = dfs(admat,j,lst)
+    return lst
+
+def bfs(admat,start,node):
+    lst = []
+    
+    lst.append(start)
     for i in range(1,node+1):
         for j in range(1,node+1):
             if admat[start][j] == 1 and j not in lst:
                 lst.append(j)
-                start = j
-                break
-        
-            if j == node:
-                start = or_start
+        start = lst[i-1]
     print(*lst)
-
-def bfs(admat,start,node):
-    lst = []
-
-    lst.append(start)
-    while len(lst) != node:
-        for i in range(1,node+1):
-            if admat[start][i] == 1:
-                lst.append(i)
 
 node, edge, start = map(int,input().split())
 admat = [[0]*(node+1) for i in range(node+1)]
@@ -37,4 +31,6 @@ for i in ls:
 for i in range(edge):
     admat[lst[i][0]][lst[i][1]] = 1
     admat[lst[i][1]][lst[i][0]] = 1
-dfs(admat,start, node)
+
+print(*dfs(admat,start, []))
+bfs(admat,start, node)
