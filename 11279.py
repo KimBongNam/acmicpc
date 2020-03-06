cad10e0ce9f09cb338f09a6a8377e62618568a24
BongnamKim <vbnmzx1@skku.edu>
2020-03-06 17:38:36 +0900

    class3 challenge

--- 11279.py
+++ 11279.py
@@ -1,24 +1,15 @@
 import sys
+import heapq
 N = int(input())
 13
 
 lst = [int(sys.stdin.readline()) for _ in range(N)]
-0
-1
-2
-0
-0
-3
-2
-1
-0
-0
-0
-0
-0
+
 
 ans = []
 for i in lst:
     if i == 0:
-        print(0 if len(ans)==0 else "????")
+        print(0 if len(ans)==0 else heapq._heappop_max(ans))
     else:
+        ans.append(i)
+        heapq._heapify_max(ans)
