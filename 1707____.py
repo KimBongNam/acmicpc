38a8f7bd767463a806ae01d42372cdb3da2701ec
BongnamKim <vbnmzx1@skku.edu>
2020-03-09 20:23:36 +0900

    1707

--- 1707____.py
+++ 1707____.py
@@ -1,14 +1,40 @@
 import sys
+def dfs(num, visited,a , b,p):
+    for i in graph[num]:
+        if i not in visited:
+            if p == 1:
+                visited.append(i)
+                a.add(i)
+                dfs(i, visited,a ,b,0)
+            else:
+                visited.append(i)
+                b.add(i)
+                dfs(i,visited,a,b,1)
+
+
+def dfsall(node, visited):
+    a = set()
+    b = set()
+    for k in range(1,node+1):
+        if k not in visited:
+            dfs(k, visited, a, b,0)
+    return a,b
+
 
 num = int(input())
 
 for i in range(num):
     nodes, edges = map(int, input().split())
-    graph = {n : [] for n in range(1, nodes+1)}
+    graph = [[] for _ in range(nodes+1)]
     for j in range(edges):
-        x, y = map(int, sys.stdin.readlines().split())
+        x,y = map(int, sys.stdin.readline().split())
         graph[x].append(y)
         graph[y].append(x)
+    a,b = dfsall(nodes,[])
+
+    print("YES" if len(a&b)==0 else "NO")
     
+
+
     
     
---
+++ 2178.py
@@ -0,0 +1,3 @@
+import sys
+N, M = map(int, input().split())
+maze = [sys.stdin.readline().strip() for _ in range(N)]
