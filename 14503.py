b240aed43987c407baac85bfb6e72ee3a7eeab8b
BongnamKim <vbnmzx1@skku.edu>
2020-04-08 21:42:29 +0900

    2h not wasting

--- 14503.py
+++ 14503.py
@@ -1,9 +1,7 @@
 import sys
  
 N, M = map(int, input().split())
-11 10
 r, c, d = map(int, input().split())
-7 4 0
 left = {0: [0,-1],
         1: [-1,0],
         2: [0,1],
@@ -16,17 +14,6 @@ dlst = [0,1,2,3]
 dxlst = [0,0,-1,1]
 dylst = [1,-1,0,0]
 lst = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-1 1 1 1 1 1 1 1 1 1
-1 0 0 0 0 0 0 0 0 1
-1 0 0 0 1 1 1 1 0 1
-1 0 0 1 1 0 0 0 0 1
-1 0 1 1 0 0 0 0 0 1
-1 0 0 0 0 0 0 0 0 1
-1 0 0 0 0 0 0 1 0 1
-1 0 0 0 0 0 1 1 0 1
-1 0 0 0 0 0 1 1 0 1
-1 0 0 0 0 0 0 0 0 1
-1 1 1 1 1 1 1 1 1 1
 
 cnt = 0
 
@@ -43,7 +30,6 @@ while True:
             available = True
     
     if not available:
-        d = dlst[(d+1)%4]
         dr, dc = front[d]
         r, c = r-dr, c-dc
         if lst[r][c] == 1:
@@ -53,7 +39,6 @@ while True:
             continue
 
     if lst[r+dr][c+dc] == 0:
-        print(d)
         d = dlst[d-1]
         dr, dc = front[d]
         r, c = r+dr, c+dc
