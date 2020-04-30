3df588943eb0c5730b5d6a997468e9a62668aa07
BongnamKim <vbnmzx1@skku.edu>
2020-04-30 22:19:04 +0900

    1q

--- 7576.py
+++ 7576.py
@@ -6,15 +6,8 @@ lst = [list(map(int, sys.stdin.readline.split())) for _ in range(N)]
 0 0 0 0 0 0
 0 0 0 0 0 0
 0 0 0 0 0 0
-0 0 0 0 0 0
+0 0 0 0 0 1
 
-fir = []
 for i in range(M):
-    fir.append(any(i))
-
-if not any(fir):
-    print(0)
-else:
-    for i in range(M):
-        for j in range(N):
-            
+    for j in range(N):
+        
