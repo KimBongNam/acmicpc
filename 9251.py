b4035f4e60f34b2222bb70a7dec95e778d1cfba2
BongnamKim <vbnmzx1@skku.edu>
2020-05-01 23:15:53 +0900

    1

--- 9251.py
+++ 9251.py
@@ -1,3 +1,18 @@
 A = input()
+ACAYKP
 B = input()
+CAPCAK
+l= max(A,B, key=lambda x:len(x))
+if l == A:
+    s = B
+else:
+    s = A
+dp = [[0] * (len(l)+1) for _ in range(len(s)+1)]
+for row in range(1,len(s)+1):
+    for col in range(1,len(l)+1):
+        if s[row-1] == l[col-1]:
+            dp[row][col] = dp[row-1][col-1]+1
+        else:
+            dp[row][col] = max(dp[row][col-1], dp[row-1][col])
 
+print(max(dp[-1]))
